568f10fd566bdb2d0f58ebb5105135f2daccf927
anastasia.birillo <anastasia.i.birillo@gmail.com>
2023-02-15 22:07:29 +0100

    Add more steps

---
+++ cardTrainerServer/cardTrainerServerCardSequenceGenerator/src/main/kotlin/jetbrains/kotlin/course/card/trainer/stat/StatModel.kt
@@ -0,0 +1,1 @@
+package jetbrains.kotlin.course.card.trainer.stat
---
+++ cardTrainerServer/cardTrainerServerCardSequenceGenerator/test/Tests.kt
@@ -0,0 +1,27 @@
+import models.ConstructorGetter
+import org.junit.jupiter.api.Test
+
+class Test {
+    @Test
+    fun cardSequenceGeneratorTestClassTest() {
+        val clazz = cardSequenceGeneratorTestClass.checkBaseDefinition()
+        cardSequenceGeneratorTestClass.checkNoConstructors(clazz)
+        cardSequenceGeneratorTestClass.checkDeclaredMethods(clazz)
+    }
+
+    @Test
+    fun cardTestClassTest() {
+        val clazz = cardTestClass.checkBaseDefinition()
+        cardTestClass.checkFieldsDefinition(clazz)
+
+        cardTestClass.checkConstructors(
+            clazz,
+            listOf(
+                ConstructorGetter(
+                    parameterTypes = listOf(String::class.java, String::class.java),
+                    toAddDefaultConstructorMarker=true
+                ),
+            )
+        )
+    }
+}
