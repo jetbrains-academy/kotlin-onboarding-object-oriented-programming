be799c7cb52fac7aa6e936fd426b9048160b8358
anastasia.birillo <anastasia.i.birillo@gmail.com>
2023-03-14 13:57:41 +0100

    Move type-aliases in the Codenames project

--- codenamesServer/codenamesServerHowToRun/src/main/kotlin/jetbrains/kotlin/course/codenames/card/CardResource.kt
+++ codenamesServer/codenamesServerHowToRun/src/main/kotlin/jetbrains/kotlin/course/codenames/card/CardResource.kt
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+typealias Card = String
+
 @RestController
 @RequestMapping("/api/cards/")
 class CardResource(val service: CardService) {
