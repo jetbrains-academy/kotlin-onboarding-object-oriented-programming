5c404a730c531ed4f128aed296d8e62d2b7c403a
anastasia.birillo <anastasia.i.birillo@gmail.com>
2023-03-16 11:02:10 +0100

    Fix alias project

--- aliasServer/aliasServerUtil/src/main/kotlin/jetbrains/kotlin/course/alias/util/Converters.kt
+++ aliasServer/aliasServerUtil/src/main/kotlin/jetbrains/kotlin/course/alias/util/Converters.kt
@@ -1,12 +1,6 @@
 package jetbrains.kotlin.course.alias.util
 
-import alias.JsCard
 import alias.JsTeam
-import jetbrains.kotlin.course.alias.card.Card
 import jetbrains.kotlin.course.alias.team.Team
 
-fun Card.toJsCard(): JsCard = TODO("Not implemented yet")
-
 fun Team.toJsTeam(): JsTeam = TODO("Not implemented yet")
-
-fun List<Team>.toArrayJsTeams(): List<JsTeam> = TODO("Not implemented yet")
