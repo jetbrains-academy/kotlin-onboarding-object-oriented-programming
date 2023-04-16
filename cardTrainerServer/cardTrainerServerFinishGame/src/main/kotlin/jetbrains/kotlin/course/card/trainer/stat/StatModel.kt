ab7838c27bb5d0ebb089c888ec5ba9b409b4c836
anastasia.birillo <anastasia.i.birillo@gmail.com>
2023-04-16 17:32:57 +0200

    Rename card trainer project

--- cardTrainerServer/cardTrainerServerFinishGame/src/main/kotlin/jetbrains/kotlin/course/card/trainer/stat/StatModel.kt
+++
@@ -1,8 +0,0 @@
-package jetbrains.kotlin.course.card.trainer.stat
-
-import jetbrains.kotlin.course.card.trainer.card.Back
-
-data class Stat(
-    val knownBacks: List<Back>,
-    val unknownBacks: List<Back>,
-)
--- memoryTrainerFrontend/src/components/screens/StartScreen.tsx
+++ memoryTrainerFrontend/src/components/screens/StartScreen.tsx
@@ -24,7 +24,7 @@ export default function StartScreen({
     return (
         <div className="App-cards-container App-min-height">
             <img src={logo} className="App-covers" alt="covers"/>
-            <p className="App-big-name font-link-bold">Card Trainer</p>
+            <p className="App-big-name font-link-bold">Memory Trainer</p>
             <p className="App-small-name font-link-base">by Kotlin Course</p>
             <div className="App-buttons-container App-display-flex">
                 <button className="App-button-base App-button-start" onClick={() => {
